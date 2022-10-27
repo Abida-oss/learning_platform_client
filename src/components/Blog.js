158351c616e8cbec5398f489fb030730d86a4501
Abida-oss <abidaakther0099@gmail.com>
2022-10-27 22:10:07 +0600

    q2q4

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,26 +5,34 @@ const Blog = () => {
         <div><br></br>
             <div className="card w-100 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">what is cors?</h2>
+                    <h2 className="card-title">What is cors?</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p><br></br>
                 </div>
             </div><br></br>
             <div className="card w-100 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">what is cors?</h2>
+                    <h2 className="card-title"> Why are you using firebase? What other options do you have to implement authentication?</h2>
+                    <p className="card-actions justify-start">The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience.<br></br>
+                        5 Common Authentication Types:
+                        1.Password-based authentication. Passwords are the most common methods of authentication.<br></br>
+                        2.Multi-factor authentication. <br></br>
+                        3.Certificate-based authentication. <br></br>
+                        4.Biometric authentication. <br></br>
+                        5.Token-based authentication.
+                    </p><br></br>
+                </div>
+            </div> <br></br>
+            <div className="card w-100 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">How does the private route work?</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p><br></br>
                 </div>
             </div> <br></br>
-             <div className="card w-100 bg-base-100 shadow-xl">
+            <div className="card w-100 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">what is cors?</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p><br></br>
-                </div>
-            </div> <br></br>
-             <div className="card w-100 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">what is cors?</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p><br></br>
+                    <h2 className="card-title"> What is Node? How does Node work?</h2>
+                    <p className="card-actions justify-start">
+                        Node. js is an open-source, cross-platform JavaScript runtime environment and library for running web applications outside the client's browser.It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</p><br></br>
                 </div>
             </div><br></br>
         </div>
